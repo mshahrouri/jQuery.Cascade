cba757bbe10a5d55b227f49aee5f00a0f1b710d9
MShahrouri <swe_shahrouri@live.com>
2013-06-22 03:48:52 +0300

    updating the readme file and adding the cascade js file

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-jQuery.Cascade
-==============
+# jQuery Cascade Plugin
 
-a cascade drop down jQuery plugin
+this is a simple jQuery plugin for cascading drop downs with the following features:
+* filling a single drop down depending on multiple drop downs.
+* adding extra arguments.
+* specifying a request type (GET or POST). 
+* specifying a function to be executed before starting the request.
+* specifying a function to be executed after completing the request.
+
+
+## Options:
+
+#### dependent (required):
+the ID of the dependent drop down.
+
+#### url (required):
+the url from where to get the data.
+
+#### textKey (required):
+the key that will be used for getting the text of an item after parsing the returned result.
+
+#### valueKey (required):
+the key that will be used for getting the value of an item after parsing the returned result.
+
+#### argName (required):
+the argument name to be used when sending request.
+
+#### onError:
+the function to be executed if an error occured while sending the request.
+
+#### onComplete:
+the function to be executed after request is complete.
+
+#### beforeRequest
+the function to be executed before the request starts.
+
+#### defualtItemText:
+the text to be used for the default item in the dependent drop down.
+
+#### extraParam:
+an object that contains the extra parameters to be send with the request (for example `{param1 : value1}`).
+
+#### additionalDDL:
+an array of objects (just the argument name and the ID of the drop down) that represents the additional drop downs to send its values with the request (for example `[{ argName: "val2", ID: "additionalDDL1" }, { argName: "val3", ID: "additionalDDL2" }]`).
+
+#### requestType:
+a string that represents the request type (GET or POST), the default request type is GET.
+
+## Sample usage:
+
+```javascript
+$("#Combo2").cascade({
+    url: "/Data/GetData",
+    dependendent: "Combo3",
+    textKey: "Text",
+    valueKey: "Value",
+    defualtItemText: "select an item",
+    argName: "val1",
+    additionalDDL: [{ argName: "val2", ID: "Combo1" }],
+    beforeRequest: function () { alert("before satrt"); },
+    onComplete: function () { alert("complete"); },
+    requestType: "POST"
+});
+```
---
+++ jquery.cascade.0.1.js
@@ -0,0 +1,95 @@
+﻿/*******************************************************
+** this plugin was created by Mohammad Shahrouri (swe_shahrouri@live.com)
+** https://github.com/mshahrouri/jQuery.Cascade
+** you can use this plugin in any way you want
+*******************************************************/
+
+(function ($) {
+    $.extend($.fn, {
+        cascade: function (options) {
+            if (options.dependent == undefined)
+                throw "options.dependent is undefined";
+            if (options.url == undefined)
+                throw "options.url is undefined";
+            if (options.textKey == undefined)
+                throw "options.textKey is undefined";
+            if (options.valueKey == undefined)
+                throw "options.valueKey is undefined";
+            if (options.argName == undefined)
+                throw "options.argName is undefined";
+
+
+            var dependentDDL = $("#" + options.dependent);
+            debugger;
+            return this.each(function () {
+                var source = $(this);
+                source.change(function () {
+                    var onErrorFunc = function () { };
+                    if (options.onError != undefined) {
+                        onErrorFunc = options.onError;
+                    }
+                    
+                    var onCompleteFunc = function () { };
+                    if (options.onComplete != undefined) {
+                        onCompleteFunc = options.onComplete;
+                    }
+                    
+                    var beforeRequestFunc = function () { };
+                    if (options.beforeRequest != undefined) {
+                        beforeRequestFunc = options.beforeRequest;
+                    }
+                    beforeRequestFunc();
+                    
+                    if (options.defualtItemText == undefined)
+                    {
+                        options.defualtItemText = "";
+                    }
+
+                    dependentDDL.empty().append(new Option(options.defualtItemText, "-1")).val("-1");
+
+                    var data = '{"' + options.argName + '":"' + source.val() + '"';
+
+                    if (options.extraParam != undefined) {
+                        for (var key in options.extraParam) {
+                            data += ',"' + key + '":"' + options.extraParam[key] + '"';
+                        }
+                    }
+
+                    if (options.additionalDDL != undefined && options.additionalDDL.length > 0) {
+                        for (var i = 0; i < options.additionalDDL.length; i++) {
+                            data += ',"' + options.additionalDDL[i].argName + '":"' + $("#" + options.additionalDDL[i].ID).val() + '"';
+                        }
+                    }
+
+                    data += '}';
+
+                    data = $.parseJSON(data);
+
+                    var requestType = options.requestType == undefined ? "GET" : options.requestType;
+
+                    $.ajax(
+                        {
+                            type: requestType,
+                            url: options.url,
+                            data: data,
+                            dataType: "json",
+                            cache: false,
+                            success: function (result) {
+                                if (result != undefined && result.length > 0) {
+                                    for (var j = 0; j < result.length; j++) {
+                                        dependentDDL.append(new Option(result[j][options.textKey], result[j][options.valueKey]));
+                                    }
+                                }
+                            },
+                            error: function (error) {
+                                onErrorFunc(error);
+                            },
+                            complete: function() {
+                                onCompleteFunc();
+                            }
+                        });
+                });
+            });
+        }
+    });
+})(jQuery);
